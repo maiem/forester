14aeeb6eb9e70178803a847dae69a4e99ccb1161
Maianh0101 <hathimaianh03@gmail.com>
2021-04-13 17:17:30 +0700

    update slide

--- js/slide.js
+++ js/slide.js
@@ -1,13 +1,20 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-      loop: true,
-      dots:true,
+$(document).ready(function () {
+  
+  $("#team").owlCarousel({
+    loop: true,
+    dots: true,
+    item: 3,
+  });
 
-    });
+  $("#quotes").owlCarousel({
+    loop: true,
+    dots: true,
+    singleItem:true,
+    item:1,
+  });
 
-    // $(".owl-carousel slide1").owlCarousel({
-    //   loop: true,
-    //   dots:true,
-    //   item: 1,
-    // });
+  $("#client").owlCarousel({
+    dots: false,
+    item: 5,
+  });
 });
